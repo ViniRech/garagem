10c2d10283bfc587cd861fa730a4fdf8c0a8edad
ViniRech <viniciusrech2007@gmail.com>
2025-06-06 19:02:24 -0300

    refactor: apresentação dos nomes das tabelas no admin

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Acessorio(models.Model):
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100, verbose_name="Descrição")
 
     def __str__(self):
         return f"{self.id} - {self.descricao}"
+
+    class Meta:
+        verbose_name = "Acessório"
--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -7,10 +7,13 @@ from .modelo import Modelo
 
 class Veiculo(models.Model):
     ano = models.IntegerField(default=0, null=True)
-    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="Preço", null=True)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True)
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True)
-    acessorios = models.ManyToManyField(Acessorio, related_name="veiculos", blank=True)
+    acessorios = models.ManyToManyField(Acessorio, related_name="veiculos", verbose_name="Acessórios", blank=True)
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.modelo.nome} {self.cor.nome} {self.ano}"
+
+    class Meta:
+        verbose_name = "Veículo"
